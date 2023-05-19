aa182080dcdec34deeb42fa279263ccf07770e25
ATEEQ-UR-REHMAN2417 <128061817+ATEEQ-UR-REHMAN2417@users.noreply.github.com>
2023-05-19 07:07:37 -0700

    Add files via upload

---
+++ JS ASSIGNMENT/app.js
@@ -0,0 +1,148 @@
+// chapter no 01 "alert"03
+// question no 01
+// alert("write first name");
+// alert("write last name");
+// alert("write email address");
+// alert("write phone number");
+// alert("write password");
+// question no 2
+// alert("you are learning java script")
+// question no 3
+// alert("you will be a best programmer in future");
+// chapter no 02 "variables"
+// chapter no 01
+// var cityName = ("kaRachi");
+// console.log(cityName);
+// question no o2
+// var Mychoice 
+// Mychoice = ("buy vezzel")
+// console.log(Mychoice);
+// question no 03
+// var teamName 
+// alert("my team name is pakistan");
+// question no 04
+// var Bestman
+// Bestman = ("my father")
+// console.log(Bestman);
+// chapter 03 "variable for numbers"
+// question no 01
+// var caseQty
+// caseQty = 144
+// console.log(caseQty)
+// question no 02
+// var num = "9";
+// question no 03
+// var a = 2
+// var b = 3
+// console.log(a+b);
+// question 05
+// var merchTotal = 100;
+// var shippingCharge = 200;
+// var totalOrder = merchTotal + shippingCharge ;
+// console.log(totalOrder)
+// CHAP 04 "variable names legal and illegal"
+    // Question 01
+    // var productcost = 3.45;
+
+    // Question2
+    // REWRITE
+    // var nameOFband;
+
+    // Q3
+    //  var myArr = 72;
+
+    //  Q4
+    // var ateeqURrehman;
+
+    // Q5
+    // no javascript keywords no wordspaces
+    // all things like letters, numbers, dollar sign etc  
+
+    // CHAP5 QUESTIONS
+    // Q1
+    // % IS THE SYMBOL THAT GIVES THE REMINDER THAT ONE NUMBER IS DIVIDED BY ANOTHER
+
+    // Q2
+    // var numb=20%6;
+    // console.log(numb);
+
+    // Q3
+    // var largeNum =1000*2000;
+    // console.log(largeNum);
+
+    // Q4
+    // var n = 66;
+    // var m = 22;
+    // o = n-m;
+// console.log(o);
+
+// Q5
+// var multi = 11*11;
+// alert("THE ANSWER IS 121 ");
+
+// CHAP6 QUESTIONS
+// Q1
+
+// var x = 92;
+// x=92+1;
+// console.log(x);
+
+// Q2
+// var X = 100;
+// var Y =1;
+// Z = X - Y;
+// console.log(Z);
+
+// Q3&4
+// var x = 50;
+// var y = x++;
+// console.log(y);
+
+// var z=--y;
+// console.log(z);
+ 
+// Q5&6
+
+// var numB= 1;
+// var newnumb=++numB;
+
+// var nUmB= 1;
+// var neWnumb=--nUmB;
+
+// Q7
+
+// var newno=52;
+// g = ++newno;
+
+// CHAP7
+// Q1
+// var calculatednum = 2 + (2*6);
+// console.log(calculatednum);
+
+// Q2
+// var calculatednUm = (2+2)*6;
+// console.log(calculatednUm);
+ 
+// Q3
+// var calculatedNUm = (2+2)*(4+2);
+// console.log(calculatedNUm);
+
+// Q4
+// var cAlculatedNUm = ((2+2)*4)+2;
+// console.log(cAlculatedNUm);
+
+// Q5
+// var staTE = 2 +2*4+10;
+// console.log(staTE);
+
+// var statE = 4/2 *4;
+// console.log(statE)
+// chapter 09
+// question no 01
+// var user = prompt("write your first name")
+// question no 02
+// var country = ("whats is your country");
+// var answ = ("china")
+// var prompt = prompt("country,answ")
+// question no 03
+// var yourName = prompt("enter your name");
